593279bdcbf05e06381ce7e1e421d9272cad01bb
Oğuzhan <ookuyan@gmail.com>
2019-03-22 17:10:12 +0300

    edit: bug fixes

--- __init__.py
+++ __init__.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+"""TÜBİTAK National Observatory Astronomy Library.
+
+An Astronomical Reduction and Analysis Tool.
+
+
+Created by Oğuzhan OKUYAN & Yücel KILIÇ - 2019
+
+(oguzhan.okuyan@tubitak.gov.tr & yucel.kilic@tubitak.gov.tr)
+
+"""
--- reduction/core.py
+++ reduction/core.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 
-import numpy as np
+__all__ = ['image_combine', 'bias_combine', 'dark_combine', 'flat_combine']
+
+from ccdproc import CCDData
+
+
+# Generic image combine function.
+# This will be bases for all bias/dark/flat combine methods
+def image_combine(bdf, method='average', masks=None,
+                  trim=None, return_ccddata=True,
+                  output_fname='master_image.fits',
+                  gain=None, read_noise=None, **kwargs):
+    pass
+
+
+def bias_combine():
+    pass
+
+
+def dark_combine():
+    pass
+
+
+def flat_combine():
+    pass
+
+
