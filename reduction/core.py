6bebe27096db299aaf1454597780ea267dca5936
yucelkilic <yucelkilic@myrafproject.org>
2019-03-24 00:36:08 +0300

    add: combine module's intro

--- reduction/core.py
+++ reduction/core.py
@@ -2,16 +2,33 @@
 
 __all__ = ['image_combine', 'bias_combine', 'dark_combine', 'flat_combine']
 
-from ccdproc import CCDData
-
+from astropy.stats import mad_std
+from ccdproc import ImageFileCollection
+from ccdproc import combine
+import numpy as np
 
 # Generic image combine function.
 # This will be bases for all bias/dark/flat combine methods
-def image_combine(bdf, method='average', masks=None,
+
+
+def image_combine(img_list, method='average', masks=None,
                   trim=None, return_ccddata=True,
                   output_fname='master_image.fits',
                   gain=None, read_noise=None, **kwargs):
-    pass
+
+    combined_images = combine(img_list,
+                              output_file=output_fname,
+                              method=method,
+                              sigma_clip=True,
+                              sigma_clip_low_thresh=5,
+                              sigma_clip_high_thresh=5,
+                              sigma_clip_func=np.ma.median,
+                              signma_clip_dev_func=mad_std)
+
+    if isinstance(output_fname, str) or isinstance(output_fname, unicode):
+        return True
+    elif output_fname is None:
+        return combined_images
 
 
 def bias_combine():
@@ -24,5 +41,3 @@ def dark_combine():
 
 def flat_combine():
     pass
-
-
