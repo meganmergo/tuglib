43d5c034165b6aa776a52629e3dae22d0289f25d
Oğuzhan <ookuyan@gmail.com>
2019-03-24 03:27:57 +0300

    add: FitsCollection

--- reduction/core.py
+++ reduction/core.py
@@ -1,20 +1,89 @@
 #!/usr/bin/env python
 
-__all__ = ['FitsCollection', 'image_combine', 'bias_combine',
+__all__ = ['FitsCollection', 'make_mask', 'image_combine', 'bias_combine',
            'dark_combine', 'flat_combine']
 
 from .helper import FitsCollection
 from ccdproc import CCDData
 
 
+def make_mask(shape, area):
+    """Make a mask specific to the given area.
+
+    Parameters
+    ----------
+
+    shape : tuple
+        Size of mask (m, n)
+
+    area : str
+        The area to be masked.
+
+    Returns
+    -------
+    m : np.array
+        Masked area.
+
+    Examples
+    --------
+
+    >>> m = make_mask((1024, 1024), '[100:200, 23:55]')
+    """
+
+    m = np.full(shape, False, dtype=bool)
+    exec('m' + area + ' = True')
+
+    return m
+
+
 # Generic image combine function.
 # This will be bases for all bias/dark/flat combine methods
 def image_combine(images, method='median', masks=None,
                   trim=None, return_ccddata=True,
-                  output_fname='master_image.fits',
+                  output='master_image.fits',
                   gain=None, read_noise=None, **kwargs):
 
-    pass
+    if not isinstance(images, FitsCollection):
+        raise TypeError(
+            "'images' should be a 'FitsCollection' object.")
+
+    if method not in ('average', 'median', 'sum'):
+        raise ValueError(
+            "'method' should be 'average', 'median' or 'sum'.")
+
+    if masks is not None:
+        if not isinstance(masks, list):
+            raise TypeError("'masks' should be a 'list' object.")
+
+    if trim is not None:
+        if not isinstance(trim, str):
+            raise TypeError("'trim' should be a 'str' object.")
+
+    if not isinstance(return_ccddata, bool):
+        raise TypeError("'return_ccddata' should be a 'bool' object.")
+
+    if not isinstance(output, str):
+        raise TypeError("'output' should be a 'str' object.")
+
+    if not isinstance(gain, float):
+        raise TypeError(
+            "'gain' should be a 'float' object.")
+
+    if not isinstance(read_noise, float):
+        raise TypeError(
+            "'read_noise' should be a 'float' object.")
+
+    if masks is not None:
+        shape = next(images.hdus(**kwargs)).shape
+        mask = make_mask(shape, masks[0])
+        for m in masks[1:]:
+            tmp_mask = make_mask(shape, m)
+            mask |= tmp_mask
+    else:
+        mask = None
+
+    master_list = list()
+
 
 
 def bias_combine():
--- reduction/helper.py
+++ reduction/helper.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
-__all__ = ['FitsCollection']
+__all__ = ['FitsCollectionTest']
 
 from os import path
 from glob import glob
-from ccdproc import ImageFileCollection
+
+from astropy import units as u
+from astropy.table import Table
+from astropy.io import fits
+
+from ccdproc import ImageFileCollection, CCDData
 
 
-# To make recursive file search in given directory.
-class FitsCollection(ImageFileCollection):
+FILE_EXTENSIONS = ('fit', 'fits', 'fit.gz', 'fits.gz', 'fit.zip', 'fits.zip')
+
+
+# It doesn't work well!
+# I will write completely new class for this purpose.
+# Helper class for ccdproc.ImageFileCollection
+class FitsCollectionTest(ImageFileCollection):
 
     def __init__(self, **kwargs):
         if 'location' in kwargs:
@@ -17,4 +27,49 @@ class FitsCollection(ImageFileCollection):
             filenames = glob(directory, recursive=True)
             kwargs['filenames'] = filenames
 
-        super(FitsCollection, self).__init__(**kwargs)
+        super(FitsCollectionTest, self).__init__(**kwargs)
+
+
+# New class for recursive image collection.
+class FitsCollection(object):
+    """FITS Image Collection.
+    . . .
+    """
+
+    def __init__(self, location=None, file_extension='fits', **kwargs):
+
+        if not isinstance(location, (None, str)):
+            raise TypeError(
+                "'location' should be 'None' or 'str' object.")
+
+        if not isinstance(file_extension, (str, tuple, list)):
+            raise TypeError(
+                "'file_extension' should be 'str', 'tuple' or 'list' object.")
+
+        if file_extension not in FILE_EXTENSIONS:
+            raise ValueError(
+                "'file_extension' should be "
+                "'fit, fits, fit.gz, fits.gz, fit.zip, fits.zip'.")
+
+        self.location = location
+        self._file_extension = file_extension
+        self.filenames = list()
+        self.collection = Table()
+
+        if location is not None:
+            self.init()
+
+    def init(self):
+        directory = path.join(self.location, '**', '*.*')
+        self.filenames = glob(directory, recursive=True)
+
+        for filename in self.filenames:
+            header = fits.getheader(filename)
+            # header = ccd.meta.cards
+
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
