bff9b354ccf0dae3cab30cebd313b920caa85679
Oğuzhan <ookuyan@gmail.com>
2019-03-25 20:10:47 +0300

    edit: bug fixes

--- reduction/helper.py
+++ reduction/helper.py
@@ -2,8 +2,6 @@
 
 __all__ = ['FitsCollection']
 
-import numpy as np
-
 from os import path
 from glob import glob
 
@@ -52,7 +50,6 @@ def get_fits_header(filename):
 
 
 # It doesn't work well!
-# I will write completely new class for this purpose.
 # Helper class for ccdproc.ImageFileCollection
 class FitsCollectionTest(ImageFileCollection):
 
@@ -66,7 +63,6 @@ class FitsCollectionTest(ImageFileCollection):
         super(FitsCollectionTest, self).__init__(**kwargs)
 
 
-# New class for recursive image collection.
 class FitsCollection(object):
     """FITS Image Collection
     (A ccdproc.ImageFileCollection alternative)
@@ -83,12 +79,47 @@ class FitsCollection(object):
         Image extensions. Should be one of 'fit', 'fits', 'fit.gz',
         'fits.gz', 'fit.zip', 'fits.zip'
 
-    Returns
-    -------
+    Examples
+    --------
 
+    >>> # Get all fits file on given directory.
+    >>> images = FitsCollection(location='/Users/oguzhan/tmp/20180901N/')
+    >>>
+    >>> # Select 'filename' and 'exptime' columns from collection
+    >>> # where object keyword equal 'BIAS'.
+    >>> biases = images[images['OBJECT'] == 'BIAS']['filename', 'EXPTIME']
+    >>> print(biases)
+                        filename                     EXPTIME
+    ------------------------------------------------ -------
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0001.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0002.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0003.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0004.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0005.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0006.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0007.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0008.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0009.fits     0.0
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0010.fits     0.0
+    >>>
+    >>> # Get ccds object from collection.
+    >>> ccds = images(biases)
+    >>>
+    >>> for i, ccd in enumerate(ccds):
+    >>>     print(biases['filename'][i], '%.3f' % np.average(ccd.data))
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0001.fits 496.991
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0002.fits 496.999
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0003.fits 497.006
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0004.fits 496.996
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0005.fits 497.001
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0006.fits 497.004
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0007.fits 497.000
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0008.fits 496.994
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0009.fits 496.987
+    /Users/oguzhan/tmp/20180901N/BDF/Bias__0010.fits 496.980
     """
 
-    def __init__(self, location, file_extension='fits'):
+    def __init__(self, location, file_extension=None):
 
         if not isinstance(location, str):
             raise TypeError(
