8291f0463368a7569aa33bcc6b4b72d34f00f081
Oğuzhan <ookuyan@gmail.com>
2019-04-22 01:39:08 +0300

    add: some test for config module

---
+++ tuglib/config/core.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+
+class Telescope(object):
+
+    def __init__(self):
+        pass
+
+
+class Detector(object):
+
+    def __init__(self):
+        pass
+
+
+class Filter(object):
+
+    def __init__(self):
+        pass
+
+
+class Grism(object):
+
+    def __init__(self):
+        pass
+
+
+class Slit(object):
+
+    def __init__(self):
+        pass
+
+
+class CalibrationLamp(object):
+
+    def __init__(self):
+        pass
