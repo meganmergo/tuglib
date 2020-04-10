fd14a0a64a8fdfacc1c944005835bbbab5385986
Yucel Kilic <yucelkilic@myrafproject.org>
2020-04-10 23:38:05 +0300

    add: annulus&dannulus radius and aperture background

--- tuglib/analysis/core.py
+++ tuglib/analysis/core.py
@@ -4,12 +4,16 @@ __all__ = ['phot']
 
 import types
 
-from photutils import CircularAperture
+from astropy.stats import sigma_clipped_stats
 from photutils import aperture_photometry
+from photutils import CircularAperture, CircularAnnulus
 from astropy.table import Table, vstack
+from astropy import units as u
+
+import numpy as np
 
 # Generic photometry function
-def phot(images, positions=None, radius=3.0, output=None):
+def phot(images, positions=None, radius=3.0, annulus=6.0, dannulus=8.0, output=None):
     """
     Basic circular aperture photometry of given images.
 
@@ -23,6 +27,22 @@ def phot(images, positions=None, radius=3.0, output=None):
         an array with shape Nx2, where N is the number of positions.
 
         Default is 'None'.
+
+
+    radius : list or float
+        The aperture radius of a source.
+
+        Default is '3.0'.
+
+    annulus : float
+        The circular inner annulus aperture radius of a source.
+
+        Default is '6.0'.
+
+    dannulus : float
+        The circular outer annulus aperture radius of a source.
+
+        Default is '8.0'.
 
     output : None or str
         If it is None, function returns just Table.
@@ -54,6 +74,12 @@ def phot(images, positions=None, radius=3.0, output=None):
     if not isinstance(radius, (list, float, int)):
         raise TypeError("'radius' should be a 'list' or 'float' object.")
 
+    if not isinstance(annulus, (float, int)):
+        raise TypeError("'radius' should be a 'int' or 'float' object.")
+
+    if not isinstance(dannulus, (float, int)):
+        raise TypeError("'radius' should be a 'int' or 'float' object.")
+
     if not isinstance(output, (type(None), type(str))):
         raise TypeError("'output' should be 'None' or 'str' objects.")
 
@@ -68,8 +94,22 @@ def phot(images, positions=None, radius=3.0, output=None):
     ccds_phot_list = []
     for ccd in ccds:
         aperture = CircularAperture(positions, r=radius)
-        phot_table = aperture_photometry(ccd, aperture)
-        phot_table['aperture_sum'].info.format = '%.8g'
-        ccds_phot_list.append(phot_table)
+        annulus_aperture = CircularAnnulus(positions, r_in=annulus, r_out=dannulus)
+        annulus_masks = annulus_aperture.to_mask(method='center')
+
+        bkg_median = []
+        for mask in annulus_masks:
+            annulus_data = mask.multiply(ccd)
+            annulus_data_1d = annulus_data[mask.data > 0]
+            _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
+            bkg_median.append(median_sigclip)
+        bkg_median = np.array(bkg_median)
+        aphot = aperture_photometry(ccd, aperture)
+        aphot['annulus_median'] = bkg_median * u.adu
+        aphot['aper_bkg'] = bkg_median * aperture.area * u.adu
+        aphot['aper_sum_bkgsub'] = aphot['aperture_sum'] - aphot['aper_bkg']
+        for col in aphot.colnames:
+            aphot[col].info.format = '%.8g'  # for consistent table output
+        ccds_phot_list.append(aphot)
 
     return vstack(ccds_phot_list)
