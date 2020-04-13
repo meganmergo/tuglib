7ab5c9d9c7d5e8060f09b4529cb7ea2c538bcb7f
Yucel Kilic <yucelkilic@myrafproject.org>
2020-04-13 05:21:29 +0300

    add: phot updated

--- tuglib/analysis/core.py
+++ tuglib/analysis/core.py
@@ -4,19 +4,24 @@ __all__ = ['phot']
 
 import types
 
-from astropy.stats import sigma_clipped_stats
 from photutils import aperture_photometry
 from photutils import CircularAperture, CircularAnnulus
-from astropy.table import Table, vstack
+from astropy.stats import sigma_clipped_stats
+from astropy.stats import SigmaClip
+from photutils import Background2D, MedianBackground
+from photutils.utils import calc_total_error
+from astropy.table import vstack
+from astropy.nddata import StdDevUncertainty
 from astropy import units as u
 
 import numpy as np
 
-from .helper import radec_to_pixel_coordinate, get_object_coordinate_from_simbad
-
 
 # Generic photometry function
-def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=None):
+def phot(images, positions=None, aperture_radius=6.0, annulus=8.0, dannulus=10.0, method='exact', bg_method="median",
+         bg_box_size=50,
+         gain=1.1,
+         output=None):
     """
     Basic circular aperture photometry of given images.
 
@@ -32,7 +37,7 @@ def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=
         Default is 'None'.
 
 
-    radius : list or float
+    aperture_radius : list or float
         The aperture radius of a source.
 
         Default is '3.0'.
@@ -46,6 +51,46 @@ def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=
         The circular outer annulus aperture radius of a source.
 
         Default is '8.0'.
+
+    method : {'exact', 'center', 'subpixel'}, optional
+        The method used to determine the overlap of the aperture on the
+        pixel grid.  Not all options are available for all aperture
+        types.  Note that the more precise methods are generally slower.
+        The following methods are available:
+
+            * ``'exact'`` (default):
+                The the exact fractional overlap of the aperture and
+                each pixel is calculated.  The returned mask will
+                contain values between 0 and 1.
+
+            * ``'center'``:
+                A pixel is considered to be entirely in or out of the
+                aperture depending on whether its center is in or out of
+                the aperture.  The returned mask will contain values
+                only of 0 (out) and 1 (in).
+
+            * ``'subpixel'``:
+                A pixel is divided into subpixels (see the ``subpixels``
+                keyword), each of which are considered to be entirely in
+                or out of the aperture depending on whether its center
+                is in or out of the aperture.  If ``subpixels=1``, this
+                method is equivalent to ``'center'``.  The returned mask
+                will contain values between 0 and 1.
+
+    bg_method: {'mean', 'median'}, optional
+        The statistic used to calculate the background.
+        All measurements are sigma clipped.
+
+    bg_box_size: int
+    Selecting the box size requires some care by the user. The box size
+    should generally be larger than the typical size of sources in the
+    image, but small enough to encapsulate any background variations. For
+    best results, the box size should also be chosen so that the data are
+    covered by an integer number of boxes in both dimensions. More information:
+    https://github.com/Gabriel-p/photpy/blob/master/IRAF_compare/aperphot.py
+
+    gain: float
+        CCD gain value.
 
     output : None or str
         If it is None, function returns just Table.
@@ -63,7 +108,7 @@ def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=
     >>> location = '/Users/ykilic/Desktop/data/'
     >>> images = FitsCollection(location, file_extension="fit")
     >>> fits_images = images.ccds(IMAGETYP='object')
-    >>> phot(fits_images, positions=[(1400.65, 1545.78)], radius=6.0, annulus=8.0, dannulus=10)
+    >>> phot(fits_images, positions=[(1400.65, 1545.78)], aperture_radius=10.0, annulus=12.0, dannulus=15)
     """
 
     if not isinstance(images, (list, types.GeneratorType)):
@@ -75,7 +120,7 @@ def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=
             raise TypeError(
                 "'positions' should be 'list' or 'None' object.")
 
-    if not isinstance(radius, (list, float, int)):
+    if not isinstance(aperture_radius, (list, float, int)):
         raise TypeError("'radius' should be a 'list' or 'float' object.")
 
     if not isinstance(annulus, (float, int)):
@@ -83,6 +128,18 @@ def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=
 
     if not isinstance(dannulus, (float, int)):
         raise TypeError("'dannulus' should be a 'int' or 'float' object.")
+
+    if not isinstance(method, str):
+        raise TypeError("'method' should be a 'str' object.")
+
+    if not isinstance(bg_method, str):
+        raise TypeError("'bg_method' should be a 'str' object.")
+
+    if not isinstance(bg_box_size, int):
+        raise TypeError("'bg_box_size' should be a 'int' or 'float' object.")
+
+    if not isinstance(gain, (float, int)):
+        raise TypeError("'gain' should be a 'float' object.")
 
     if not isinstance(output, (type(None), type(str))):
         raise TypeError("'output' should be 'None' or 'str' objects.")
@@ -100,32 +157,59 @@ def phot(images, positions=None, radius=6.0, annulus=8.0, dannulus=10.0, output=
     for ccd in ccds:
         exptime = ccd.meta['EXPTIME']
 
-        aperture = CircularAperture(positions, r=radius)
-        annulus_aperture = CircularAnnulus(positions, r_in=annulus, r_out=dannulus)
-        annulus_masks = annulus_aperture.to_mask(method='center')
+        box_xy = (bg_box_size, bg_box_size)
+        sigma_clip = SigmaClip(sigma=3.)
+        bkg_estimator = MedianBackground()
+        bkg = Background2D(ccd, box_xy, filter_size=(3, 3),
+                           sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
 
-        bkg_median = []
+        ccd.uncertainty = StdDevUncertainty(calc_total_error(ccd.data, bkg.background, gain))
+
+        aperture = CircularAperture(positions, r=aperture_radius)
+        annulus_aperture = CircularAnnulus(positions, r_in=annulus, r_out=dannulus)
+        annulus_masks = annulus_aperture.to_mask(method=method)
+
+        bkg_phot = []
         for mask in annulus_masks:
             annulus_data = mask.multiply(ccd)
             annulus_data_1d = annulus_data[mask.data > 0]
-            _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
-            bkg_median.append(median_sigclip)
-        bkg_median = np.array(bkg_median)
-        phot_table = aperture_photometry(ccd, aperture)
-        phot_table['annulus_median'] = bkg_median * u.adu
-        phot_table['aper_bkg'] = bkg_median * aperture.area * u.adu
-        phot_table['aper_sum_bkgsub'] = phot_table['aperture_sum'] - phot_table['aper_bkg']
+            mean_sigclip, median_sigclip, stddev_sigclip = sigma_clipped_stats(annulus_data_1d)
 
-        phot_table['flux'] = phot_table['aper_sum_bkgsub'] / float(exptime)
+            if bg_method == "mean":
+                sigclip = mean_sigclip
+            elif bg_method == "median":
+                sigclip = median_sigclip
+            else:
+                sigclip = median_sigclip
+
+            bkg_phot.append(sigclip)
+
+        bkg_phot = np.array(sigclip)
+        phot_table = aperture_photometry(ccd, aperture)
+        phot_table['annulus_{}'.format(bg_method)] = bkg_phot * u.adu
+        phot_table['aper_bkg'] = bkg_phot * aperture.area * u.adu
+        phot_table['residual_aperture_sum'] = phot_table['aperture_sum'] - phot_table['aper_bkg']
+
+        phot_table['flux'] = phot_table['residual_aperture_sum'] / float(exptime)
+        phot_table['flux'].info.format = '%.3f'
         phot_table['JD'] = float(ccd.meta['JD'])
         phot_table['JD'].info.format = '%.8f'
         phot_table['JD'].unit = 'd'
         phot_table['aperture_sum'].info.format = '%.3f'
-        phot_table['annulus_median'].info.format = '%.3f'
+        phot_table['aperture_sum_err'].info.format = '%.3f'
+        phot_table['annulus_{}'.format(bg_method)].info.format = '%.3f'
         phot_table['aper_bkg'].info.format = '%.3f'
-        phot_table['aper_sum_bkgsub'].info.format = '%.3f'
-        phot_table['flux'].info.format = '%.3f'
+        phot_table['residual_aperture_sum'].info.format = '%.3f'
+        phot_table = flux2mag(phot_table)
 
+        phot_table['mag_err'] = 1.0857 * (phot_table['aperture_sum_err'] / phot_table['residual_aperture_sum']) * u.mag
+        phot_table['mag_err'].info.format = '%.3f'
         ccds_phot_list.append(phot_table)
 
     return vstack(ccds_phot_list)
+
+
+def flux2mag(phot_table, zmag=25.):
+    phot_table['mag'] = (zmag - 2.5 * np.log10(np.array(phot_table['flux']))) * u.mag
+    phot_table['mag'].info.format = '%.3f'
+    return phot_table
